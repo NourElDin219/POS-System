6791f6a999b2966e156b028ebd0ee0e571847060
Nour El-Din Ahmed <nouraldin2017170481@cis.asu.edu.eg>
2022-08-27 20:51:33 +0200

    Admin

--- POS Sys/admin.Designer.cs
+++ POS Sys/admin.Designer.cs
@@ -32,17 +32,17 @@ namespace POS_Sys
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(admin));
             this.panel2 = new System.Windows.Forms.Panel();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label9 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
             this.panel2.SuspendLayout();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
@@ -65,96 +65,23 @@ namespace POS_Sys
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.DarkSlateGray;
-            this.panel1.Controls.Add(this.label9);
-            this.panel1.Controls.Add(this.label8);
-            this.panel1.Controls.Add(this.label2);
-            this.panel1.Controls.Add(this.label5);
-            this.panel1.Controls.Add(this.label6);
-            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.button6);
             this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.button5);
             this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(395, 753);
             this.panel1.TabIndex = 5;
             // 
-            // label9
-            // 
-            this.label9.ForeColor = System.Drawing.Color.White;
-            this.label9.Image = ((System.Drawing.Image)(resources.GetObject("label9.Image")));
-            this.label9.ImageAlign = System.Drawing.ContentAlignment.TopRight;
-            this.label9.Location = new System.Drawing.Point(86, 470);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(229, 23);
-            this.label9.TabIndex = 9;
-            this.label9.Text = "اعدادات المتجر";
-            this.label9.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-            // 
-            // label8
-            // 
-            this.label8.ForeColor = System.Drawing.Color.White;
-            this.label8.Image = ((System.Drawing.Image)(resources.GetObject("label8.Image")));
-            this.label8.ImageAlign = System.Drawing.ContentAlignment.TopRight;
-            this.label8.Location = new System.Drawing.Point(86, 711);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(229, 23);
-            this.label8.TabIndex = 8;
-            this.label8.Text = "تسجيل الخروج";
-            this.label8.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.label8.Click += new System.EventHandler(this.label8_Click);
-            // 
-            // label2
-            // 
-            this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Image = ((System.Drawing.Image)(resources.GetObject("label2.Image")));
-            this.label2.ImageAlign = System.Drawing.ContentAlignment.TopRight;
-            this.label2.Location = new System.Drawing.Point(86, 412);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(229, 23);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "اعدادات المستخدم";
-            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-            // 
-            // label5
-            // 
-            this.label5.ForeColor = System.Drawing.Color.White;
-            this.label5.Image = ((System.Drawing.Image)(resources.GetObject("label5.Image")));
-            this.label5.ImageAlign = System.Drawing.ContentAlignment.TopRight;
-            this.label5.Location = new System.Drawing.Point(86, 348);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(229, 23);
-            this.label5.TabIndex = 5;
-            this.label5.Text = "سجل المبيعات";
-            this.label5.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.label5.Click += new System.EventHandler(this.label5_Click);
-            // 
-            // label6
-            // 
-            this.label6.ForeColor = System.Drawing.Color.White;
-            this.label6.Image = ((System.Drawing.Image)(resources.GetObject("label6.Image")));
-            this.label6.ImageAlign = System.Drawing.ContentAlignment.TopRight;
-            this.label6.Location = new System.Drawing.Point(86, 282);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(229, 23);
-            this.label6.TabIndex = 6;
-            this.label6.Text = "المخزن";
-            this.label6.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-            // 
-            // label7
-            // 
-            this.label7.ForeColor = System.Drawing.Color.White;
-            this.label7.Image = ((System.Drawing.Image)(resources.GetObject("label7.Image")));
-            this.label7.ImageAlign = System.Drawing.ContentAlignment.TopRight;
-            this.label7.Location = new System.Drawing.Point(86, 219);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(229, 23);
-            this.label7.TabIndex = 7;
-            this.label7.Text = "المنتجات";
-            this.label7.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-            // 
             // label4
             // 
+            this.label4.ForeColor = System.Drawing.Color.White;
             this.label4.Location = new System.Drawing.Point(86, 159);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(229, 23);
@@ -202,6 +129,60 @@ namespace POS_Sys
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(89, 219);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(226, 34);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "المنتجات";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(89, 296);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(226, 34);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "المخزن";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(89, 368);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(226, 34);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "سجل المبيعات";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(89, 442);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(226, 34);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "اعدادات المستخدم";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(89, 518);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(226, 34);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "اعدادات المتجر";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(89, 676);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(226, 34);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "تسجيل الخروج";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
             // admin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -223,17 +204,17 @@ namespace POS_Sys
 
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
     }
 }
 
