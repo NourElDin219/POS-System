f5dfa5011dfe40996861f2732968894aba5f8977
Nour El-Din Ahmed <nouraldin2017170481@cis.asu.edu.eg>
2022-08-31 00:13:32 +0200

    ...

---
+++ POS Sys/Migrations/202208302156413_firstmigration.cs
@@ -0,0 +1,72 @@
+﻿namespace POS_Sys.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class firstmigration : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Categories",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Products",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        PurchasingPrice = c.Double(nullable: false),
+                        SellingPrice = c.Double(nullable: false),
+                        Quantity = c.Int(nullable: false),
+                        Discount = c.Int(nullable: false),
+                        Category_Id = c.Int(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Categories", t => t.Category_Id)
+                .Index(t => t.Category_Id);
+            
+            CreateTable(
+                "dbo.Roles",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        UserName = c.String(),
+                        Password = c.String(),
+                        Role_Id = c.Int(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Roles", t => t.Role_Id)
+                .Index(t => t.Role_Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Users", "Role_Id", "dbo.Roles");
+            DropForeignKey("dbo.Products", "Category_Id", "dbo.Categories");
+            DropIndex("dbo.Users", new[] { "Role_Id" });
+            DropIndex("dbo.Products", new[] { "Category_Id" });
+            DropTable("dbo.Users");
+            DropTable("dbo.Roles");
+            DropTable("dbo.Products");
+            DropTable("dbo.Categories");
+        }
+    }
+}
---
+++ POS Sys/Models/Database_Context.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+namespace POS_Sys.Models
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext() : base("Data Source=DESKTOP-6D2T5N4; Integrated Security=true;Initial Catalog=POS;")
+        {
+
+        }
+        public DbSet<Users> User { get; set; }
+        public DbSet<Roles> Role { get; set; }
+        public DbSet<Products> Product { get; set; }
+        public DbSet<Category> Category { get; set; }
+
+    }
+
+}
